81617ed707c9fdbbc7b15b64b20fe19d924cef0c
yangzhen19950126 <925721864@qq.com>
2016-08-27 09:11:25 +0800

    Update DownloadServlet.java

--- src/servlet/DownloadServlet.java
+++ src/servlet/DownloadServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/DownloadServlet")
 public class DownloadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
