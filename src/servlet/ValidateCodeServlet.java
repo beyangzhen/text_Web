a487bce345a27021b83885de367d57d1690fd69e
yangzhen19950126 <925721864@qq.com>
2016-08-27 09:13:09 +0800

    Update ValidateCodeServlet.java

--- src/servlet/ValidateCodeServlet.java
+++ src/servlet/ValidateCodeServlet.java
@@ -29,6 +29,7 @@ import javax.servlet.http.HttpServletResponse;
  * 
  * ?? 添加干扰线
  */
+@WebServlet("/ValidateCodeServlet")
 public class ValidateCodeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
