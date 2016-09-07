beacffe5d0cc4838354cdd9452e4ad3fa9d36ebc
yangzhen19950126 <925721864@qq.com>
2016-09-07 17:04:02 +0800

    Add files via upload

---
+++ src/jdbcAdvanced/transaction/saveProcedureTest.java
@@ -0,0 +1,34 @@
+package jdbcAdvanced.savaProcedure;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+import org.junit.Test;
+
+import jdbcAdvanced.JdbcUtil;
+
+public class saveProcedureTest {
+
+	@Test
+	public void test(){
+		Connection connection = null;
+		CallableStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = JdbcUtil.getConnection();
+			statement = connection.prepareCall("{call simpleproc (?)}");
+			statement.registerOutParameter(1, Types.VARCHAR);
+			statement.execute();
+			System.out.println(statement.getString(1));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.release(connection, statement, resultSet);
+		}
+	}
+}
