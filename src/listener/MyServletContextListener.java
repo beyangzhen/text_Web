1f83f1e25dc852a26ef3e1a7a049920b5854cfee
yangzhen19950126 <925721864@qq.com>
2016-09-13 17:45:30 +0800

    Update MyServletContextListener.java

--- src/listener/MyServletContextListener.java
+++ src/listener/MyServletContextListener.java
@@ -5,7 +5,10 @@ import javax.servlet.ServletContextListener;
 
 /**
  *  包括Application：对象初始化/销毁 和 对象属性增加/代替/删除 			    的监听
- *      Session	   ：对象初始化/销毁 和 对象属性增加/代替/删除 和 合并/绑定javaBean 的监听
+ *      Session	   ：对象初始化/销毁 和 对象属性增加/代替/删除 和 javaBean钝化（session中javaBean保存到文件中）
+ * 									  活化（从文件中取出javaBean）
+ * 									  绑定（session中保存javaBean）     
+ * 										    的监听
  *      Request    ：对象初始化/销毁 和 对象属性增加/代替/删除 			    的监听
  *      
  * /
