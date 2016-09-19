09980ae3d4e72b4917d3e90bcb99cb02e34227a3
yangzhen19950126 <925721864@qq.com>
2016-09-19 20:31:22 +0800

    Add files via upload

---
+++ src/AjaxServlet/AjaxServlet.java
@@ -0,0 +1,32 @@
+package com.wxhledu.cn.web;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ajax")
+public class AjaxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AjaxServlet() {
+        super();
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=utf-8");
+		System.out.println("method:" + request.getMethod());
+		
+		Enumeration<String> names = request.getParameterNames();
+		while (names.hasMoreElements()) {
+			String name = (String) names.nextElement();
+			System.out.println(name  + "=" + request.getParameter(name));
+		}
+		response.getWriter().write("{\"a\":1,\"b\":\"hello\"}");
+	}
+
+}
