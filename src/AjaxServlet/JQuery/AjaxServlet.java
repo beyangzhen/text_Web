03df603b03cdbf932f0d0816fded057bdb3b0dec
yangzhen19950126 <925721864@qq.com>
2016-09-20 13:48:39 +0800

    Update AjaxServlet.java

--- src/AjaxServlet/JQuery/AjaxServlet.java
+++ src/AjaxServlet/JQuery/AjaxServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/ajax")
+@WebServlet("/ajaxServlet")
 public class AjaxServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
