6bf9df6ee1d3f07f6eae1cdb918cb0c4249e8cc6
yangzhen19950126 <925721864@qq.com>
2016-09-20 13:55:39 +0800

    Add files via upload

---
+++ src/AjaxServlet/JQuery/JsonServlet.java
@@ -0,0 +1,37 @@
+package com.wxhledu.cn.web;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+WebServlet("/jsonServlet)
+public class JsonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public JsonServlet() {
+        super();
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.setContentType("text/html;charset=UTF-8");
+		response.setContentType("application/json;charset=UTF-8");
+		
+		StringBuffer url = request.getRequestURL();
+		String uri = request.getRequestURI();
+		System.out.println("url:"+url);
+		System.out.println("uri:"+uri);
+		
+		Enumeration<String> names = request.getParameterNames();
+		while (names.hasMoreElements()) {
+			String name = (String) names.nextElement();
+			System.out.println("name:" + name + "\tvalue:" + request.getParameter(name));
+		}
+		response.getWriter().write("{\"username\":\"dd\",\"password\":\"123456\",\"age\":18,\"IC\":null,\"IQ\":250}");
+	}
+
+}
